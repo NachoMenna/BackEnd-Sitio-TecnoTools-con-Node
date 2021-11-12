be8588b15d7642c39eb7aef355f8af6832b8e0ff
Ignacio Menna <nachomenna@yahoo.com.ar>
2021-11-12 13:55:06 -0300

    Tarea unidad 4

--- app.js
+++ app.js
@@ -3,13 +3,14 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const session = require('express-session');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var novedadesRouter = require ('./routes/novedades');
-var productosRouter = require ('./routes/productos');
-var nosotrosRouter = require ('./routes/nosotros');
-var contactoRouter = require ('./routes/contacto');
+var novedadesRouter = require('./routes/novedades');
+var productosRouter = require('./routes/productos');
+var nosotrosRouter = require('./routes/nosotros');
+var contactoRouter = require('./routes/contacto');
 
 
 var app = express();
@@ -24,6 +25,35 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(session({
+  secret: 'NachoMenna',
+  resave: false,
+  saveUninitialized: true,
+}))
+
+app.get('/', function (req, res) {
+  var conocido = Boolean(req.session.nombre);
+
+  res.render('index', {
+    title: 'Administrador Tecno Tools',
+    conocido: conocido,
+    nombre: req.session.nombre
+  });
+});   
+
+app.post('/ingresar', function (req, res) {
+  if (req.body.nombre) {
+    req.session.nombre = req.body.nombre
+  }
+  res.redirect('/');
+})
+
+app.get('/salir', function (req, res) {
+  req.session.destroy();
+  res.redirect('/');
+})
+
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/productos', productosRouter);
@@ -33,12 +63,12 @@ app.use('/contacto', contactoRouter);
 
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
